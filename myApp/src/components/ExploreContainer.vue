e1b3e1bfc448893bc14ea85984e7f036be4e6f53
felipaugsts <felipaugsts@gmail.com>
2021-03-13 19:43:17 -0300

    first commit

---
+++ myApp/src/components/ExploreContainer.vue
@@ -0,0 +1,47 @@
+<template>
+<ion-content>
+  <ion-card id="card" v-for="(item, index) in APIData" :key="index">
+    <ion-card-header>
+      <ion-card-title>{{item.title}}</ion-card-title>
+      <ion-card-subtitle>{{item.Company}}  |  {{item.salary}}</ion-card-subtitle> 
+    </ion-card-header>
+
+    <ion-card-content>
+      {{item.description}}
+    </ion-card-content>
+    <ion-button target="_blank" :href="item.Contact" id="btn" color="success">Acessar</ion-button>
+        <ion-button to="/tab2222" id="btn" color="primary">Informações</ion-button>
+  </ion-card>
+</ion-content>
+</template>
+
+<script>
+import { getAPI } from "@/axio-api";
+export default {
+  data() {
+    return {
+      APIData: []
+    };
+  },
+  created() {
+    getAPI
+      .get("/jobs/")
+      .then(response => {
+        console.log("Post api is getting data");
+        this.APIData = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style scoped>
+#btn {
+  margin: 10px;
+}
+#card {
+  margin-bottom: 20px;
+}
+</style>
---
+++ myApp/src/views/Tabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="home" />
+          <ion-label>Vagas</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button disabled tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="menu" />
+          <ion-label>Menu</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
+import { home, menu} from 'ionicons/icons';
+
+export default {
+  name: 'Tabs',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  setup() {
+    return {
+      home, 
+      menu, 
+    }
+  }
+}
+</script>
